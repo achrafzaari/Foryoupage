f6d5d071e4d80d26df07498ecede7ebbeb2be274
achrafzaari <132367280+achrafzaari@users.noreply.github.com>
2026-04-05 20:26:17 +0100

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
 
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
@@ -86,4 +86,3 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: 'خطأ داخلي', message: err.message });
   }
 }
-
